e211da0fc90f0c6c6f70bd9057d6e126dd27f82c
M1ST <efjennisoniv@gmail.com>
2026-02-06 14:55:53 -0800

    Completion of the RN Golf Excersize

---
+++ src/Play.js
@@ -0,0 +1,117 @@
+class Play extends Phaser.Scene {
+    constructor() {
+        super('playScene')
+    }
+
+    init() {
+        // useful variables
+        this.SHOT_VELOCITY_X = 200
+        this.SHOT_VELOCITY_Y_MIN = 700
+        this.SHOT_VELOCITY_Y_MAX = 1100
+
+        // Variables for the score and stats
+        this.shotCount = 0
+        this.score = 0
+    }
+
+    preload() {
+        this.load.path = './assets/img/'
+        this.load.image('grass', 'grass.jpg')
+        this.load.image('cup', 'cup.jpg')
+        this.load.image('ball', 'ball.png')
+        this.load.image('wall', 'wall.png')
+        this.load.image('oneway', 'one_way_wall.png')
+    }
+
+    create() {
+        // add background grass
+        this.grass = this.add.image(0, 0, 'grass').setOrigin(0)
+
+        // add cup
+        this.cup = this.physics.add.sprite(width / 2, height / 10, 'cup')
+        this.cup.body.setCircle(this.cup.width / 4)
+        this.cup.body.setOffset(this.cup.width / 4)
+        this.cup.body.setImmovable(true)
+        // add ball
+        this.ball = this.physics.add.sprite(width / 2, height - height / 10, 'ball')
+        this.ball.body.setCircle(this.ball.width / 2)
+        this.ball.body.setCollideWorldBounds(true)
+        this.ball.body.setBounce(0.5)
+        this.ball.body.setDamping(true).setDrag(0.5)
+        // add walls
+        let wallA = this.physics.add.sprite(0, height / 4, 'wall')
+        wallA.setX(Phaser.Math.Between(0 + wallA.width / 2, width - wallA.width / 2))
+        wallA.body.setImmovable(true)
+
+        let wallB = this.physics.add.sprite(0, height / 2, 'wall')
+        wallB.setX(Phaser.Math.Between(0 + wallB.width / 2, width - wallB.width / 2))
+        wallB.body.setImmovable(true)
+
+        // Make one of the obstacles move left and right to make it more challenging like golf with friends
+        wallB.body.setCollideWorldBounds(true)
+        wallB.body.setVelocityX(150)
+        wallB.body.setBounce(1)
+
+        this.walls = this.add.group([wallA, wallB])
+        // add one-way
+        this.oneWay = this.physics.add.sprite(width / 2, height / 4 * 3, 'oneway')
+        this.oneWay.setX(Phaser.Math.Between(0 + this.oneWay.width / 2, width - this.oneWay.width / 2))
+        this.oneWay.body.setImmovable(true)
+        this.oneWay.body.checkCollision.down = false
+
+        //Display the ui for the score and shots tracker
+        let textConfig = {
+            fontSize: '20px',
+            color: '#000',
+            backgroundColor: '#FFF'
+        }
+
+        this.scoreText = this.add.text(20, 20, 'Shots: 0 | Score: 0 | %: 0', textConfig)
+        // add pointer input
+        this.input.on('pointerdown', (pointer) => {
+            let shotDirection = pointer.y <= this.ball.y ? 1 : -1
+            this.ball.body.setVelocityX(Phaser.Math.Between(-this.SHOT_VELOCITY_X, this.SHOT_VELOCITY_X))
+            this.ball.body.setVelocityY(Phaser.Math.Between(this.SHOT_VELOCITY_Y_MIN, this.SHOT_VELOCITY_Y_MAX) * shotDirection)
+
+            // Add to the shot count
+            this.shotCount++
+            this.updateUI()
+        })
+        // cup/ball collision
+        this.physics.add.collider(this.ball, this.cup, (ball, cup) => {
+            
+            // Instead of destroying the ball, reset it to its postion and track the score
+            this.score++
+            this.updateUI()
+            this.resetBall()
+        })
+        // ball/wall collision
+        this.physics.add.collider(this.ball, this.walls)
+        // ball/one-way collision
+        this.physics.add.collider(this.ball, this.oneWay)
+    }
+
+    // Ball reset logic
+    resetBall() {
+        this.ball.setPosition(width / 2, height - height / 10)
+        this.ball.body.setVelocity(0, 0)
+    }
+
+    // UI updating for the score and stuff
+    updateUI() {
+        let percent = this.shotCount > 0 ? ((this.score / this.shotCount) * 100).toFixed(1) : 0
+        this.scoreText.setText(`Shots: ${this.shotCount} | Score: ${this.score} | %: ${percent}`)
+    }
+
+    update() {
+
+    }
+}
+/*
+CODE CHALLENGE
+Try to implement at least 3/4 of the following features during the remainder of class (hint: each takes roughly 15 or fewer lines of code to implement):
+[ ] Add ball reset logic on successful shot
+[ ] Improve shot logic by making pointer’s relative x-position shoot the ball in correct x-direction
+[ ] Make one obstacle move left/right and bounce against screen edges
+[ ] Create and display shot counter, score, and successful shot percentage
+*/
---
+++ src/main.js
@@ -0,0 +1,22 @@
+// Code Practice: RNGolf
+// Name: Ernest Jennison
+// Date: 2/1/26
+
+'use strict'
+
+let config = {
+    type: Phaser.AUTO,
+    width: 640,
+    height: 960,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            debug: true
+        }
+    },
+    scene: [ Play ]
+}
+
+let game = new Phaser.Game(config)
+
+let { width, height } = game.config
